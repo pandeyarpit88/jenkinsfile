d5a5c23d80f3966c4e338416c95beaa05a7b8069
Arpit Pandey <arpit.pandey@tavant.com>
2018-02-27 16:22:49 +0530

    changed webservice to print env variables

--- src/main/java/com/experian/api/demo/DemoApplication.java
+++ src/main/java/com/experian/api/demo/DemoApplication.java
@@ -5,12 +5,19 @@ import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 	@RequestMapping("/")
 	String home() {
-		return "Hello World!";
+        Map<String, String> env = System.getenv();
+        StringBuilder sb = new StringBuilder ();
+        for (String envName : env.keySet()) {
+            sb.append(envName).append(" : ").append("<br>");
+        }
+		return sb.toString();
 	}
 
 
